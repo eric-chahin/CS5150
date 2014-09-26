933a3ba854b2cbc10523f0098d075b02bcfbb5f7
kem226 <kemurray2011@aol.com>
2014-09-26 19:00:52 -0400

    starting prototype

--- index.php
+++ index.php
@@ -1,10 +1,142 @@
-<html>
- <head>
-  <title>Checklist Interactive</title>
- </head>
- <body>
-    
- <?php echo '<p>Hello World</p>'; ?>
- 
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bootstrap 3, from LayoutIt!</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+	
+	<link href="css/mybootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="img/favicon.png">
+  
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</head>
+
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="jumbotron">
+				<h1>
+					THIS IS YOUR CHECKLIST
+				</h1>
+				
+			</div>
+		</div>
+	</div>
+	<div class="row clearfix">
+	  <div class="row" >
+		<div class="col-md-12 column">
+			<div class="carousel slide" id="carousel-111948" data-interval="false">
+				
+				<div class="carousel-inner">
+					<div class="item">
+					   <div class= "twoyear"> 
+					      <div class= "year"> 
+						   <div class= "semester">
+						    <h4>FALL 3</h4>
+						    JUNIOR
+						  </div>
+						  <div class= "semester">
+						     <h4>SPRING 3</h4>
+						    JUNIOR
+						  </div>
+					      </div>
+					       <div class= "year"> 
+					       <div class= "semester">
+						    <h4>FALL 4</h4>
+						    SENIOR
+						  </div>
+						  <div class= "semester">
+						     <h4>SPRING 4</h4>
+						    SENIOR
+						  </div>
+					       </div>
+					  </div>
+					</div>
+					<div class="item active">
+					   <div class= "twoyear"> 
+					      <div class= "year"> 
+						  <div class= "semester">
+						    <h4>FALL 1</h4>
+						    FRESHMAN
+						  </div>
+						  <div class= "semester">
+						     <h4>SPRING 1</h4>
+						    FRESHMAN
+						  </div>
+					      </div>
+					      <div class= "year"> 
+						   <div class= "semester">
+						    <h4>FALL 2</h4>
+						    SOPHOMORE
+						  </div>
+						  <div class= "semester">
+						     <h4>SPRING 2</h4>
+						    SOPHOMORE
+						  </div>
+					      </div>
+					  </div>
+					</div>
+				</div> <a class="left carousel-control" href="#carousel-111948" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-111948" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+		</div>
+	</div>
+	  </div>
+	<div class="row clearfix">
+	  <div class="row2" >
+	  <div class="col-md-4 column">
+			  <div class= "year"> 
+						<div class= "core"> Hello1 </div>
+					    </div>
+			  </div>
+		<div class="col-md-8 column">
+			<div class="carousel slide" id="carousel-144942" data-interval="false">
+
+				<div class="carousel-inner">
+					<div class="item active">
+					    <div class= "classContainer"> 
+						<div class= "classholder"> Hello1 </div>
+					    </div>
+					</div>
+					<div class="item">
+					    <div class= "classContainer"> 
+						<div class= "classholder"> Hello2 </div>
+					    </div>
+					</div>
+					<div class="item">
+					    <div class= "classContainer"> 
+						<div class= "classholder"> Hello3 </div>
+					    </div>
+					</div>
+				</div> <a class="left carousel-control" href="#carousel-144942" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-144942" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+		    </div>
+		</div>
+	</div>
+
+</div>
+</body>
 </html>
