65dc168e568bfd2d947ce02af07aae11370c51ac
kem226 <kemurray2011@aol.com>
2014-10-03 13:16:29 -0400

    art assests being added and hexagons

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Bootstrap 3, from LayoutIt!</title>
+  <title>Checklist Interactive</title>
   <meta name="description" content="">
   <meta name="author" content="">
 
@@ -50,87 +50,121 @@
 			<div class="carousel slide" id="carousel-111948" data-interval="false">
 				
 				<div class="carousel-inner">
-					<div class="item">
-					   <div class= "twoyear"> 
-					      <div class= "year"> 
-						   <div class= "semester">
-						    <h4>FALL 3</h4>
-						    JUNIOR
-						  </div>
-						  <div class= "semester">
-						     <h4>SPRING 3</h4>
-						    JUNIOR
-						  </div>
-					      </div>
-					       <div class= "year"> 
-					       <div class= "semester">
-						    <h4>FALL 4</h4>
-						    SENIOR
-						  </div>
-						  <div class= "semester">
-						     <h4>SPRING 4</h4>
-						    SENIOR
-						  </div>
-					       </div>
-					  </div>
-					</div>
-					<div class="item active">
-					   <div class= "twoyear"> 
-					      <div class= "year"> 
-						  <div class= "semester">
-						    <h4>FALL 1</h4>
-						    FRESHMAN
-						  </div>
-						  <div class= "semester">
-						     <h4>SPRING 1</h4>
-						    FRESHMAN
-						  </div>
-					      </div>
-					      <div class= "year"> 
-						   <div class= "semester">
-						    <h4>FALL 2</h4>
-						    SOPHOMORE
-						  </div>
-						  <div class= "semester">
-						     <h4>SPRING 2</h4>
-						    SOPHOMORE
-						  </div>
-					      </div>
-					  </div>
-					</div>
-				</div> <a class="left carousel-control" href="#carousel-111948" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                                   <?php
+                                      
+                                      for ($i = 1; $i <= 8; $i++) {
+                                              //if the current semester is a start of a new carousel cell, then add the item div
+                                              if ($i == 1 || $i == 5) {
+                                                   if ($i == 1) echo ('<div class="item active">');
+                                                   else if ($i == 5) echo ('<div class="item">');
+                                                   echo ('<div class= "twoyear">');
+                                              }
+                                              //if this semester is the start of a year, put the year div in
+                                              if ($i % 2 == 1) echo ('<div class= "year">');
+                                              
+                                              //echo the semester div every time
+                                              echo ('<div class= "semester">');
+                                              
+                                              if ($i % 2 == 1) {
+                                                      $img = "img/text_fall" . (($i+1)/2) . ".png";
+                                              }
+                                              else {
+                                                      $img = "img/text_spring" . ($i/2) . ".png";
+                                              }
+                                      
+                                              echo ('<div class="semestertitle"><img src= "'.$img.'"></div>');
+                                              echo ('
+                                                     <div class="hexagon">CS 2110</div>
+                                                     <div class="hexagon">CS 4410</div>
+                                                     <div class="hexagonLeft">CS 3110</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagonLeft">CS 3110</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                   </div>
+                                                   ');
+                                              if ($i % 2 == 0) {
+                                                      echo ('</div>');
+                                              }
+                                      
+                                              if ($i == 4 || $i == 8) {
+                                                      echo ('</div></div>');
+                                              }
+                                      }
+                                      
+                                      ?>
+
+                                </div><a class="left carousel-control" href="#carousel-111948" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
 				<a class="right carousel-control" href="#carousel-111948" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 			</div>
 		</div>
 	</div>
 	  </div>
+        <div class="row clearfix">
+            <div class="row2" >
+               <div class="col-md-12 column">
+                   <div class= "core">
+                    <div class= "coretitle"><img src= "img/text_core.png"></div>
+                                                    <div class="hexagonCore">CS 2110</div>
+                                                    <div class="hexagonCore">CS 4410</div>
+                                                    <div class="hexagonCore">CS 3410</div>
+                                                    <div class="hexagonCore">CS 3410</div>
+                                                    <div class="hexagonCore">CS 3410</div>
+                                                    <div class="hexagonCore">CS 3410</div>
+                                                    <div class="hexagonCore">CS 2110</div>
+                                                    <div class="hexagonCore">CS 4410</div>
+                                                    <div class="hexagonCore">CS 3410</div>
+                                                    <div class="hexagonCore">CS 3410</div>
+                                                
+                   </div>
+                
+               </div>
+            </div>
+        </div>
+        
 	<div class="row clearfix">
 	  <div class="row2" >
 	  <div class="col-md-4 column">
-			  <div class= "year"> 
-						<div class= "core"> Hello1 </div>
-					    </div>
+						<div class= "search"> Search </div>
 			  </div>
 		<div class="col-md-8 column">
 			<div class="carousel slide" id="carousel-144942" data-interval="false">
 
 				<div class="carousel-inner">
-					<div class="item active">
+                                 <?php 
+                                   $classnumber = 54;
+                                   for ($i = 0; $i <= ($classnumber); $i+=18) {
+                                    if ($i == 0) echo ('<div class="item active">');
+                                    else echo ('<div class="item">');
+                                    
+                                          echo ('
 					    <div class= "classContainer"> 
-						<div class= "classholder"> Hello1 </div>
+                                                <div class= "coursetitle"><img src= "img/text_courses.png"></div>
+                                                     <div class="hexagon">CS 2110</div>
+                                                     <div class="hexagon">CS 4410</div>
+                                                     <div class="hexagon">CS 3110</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagonLeft">CS 3410</div>
+                                                     <div class="hexagon">CS 2110</div>
+                                                     <div class="hexagon">CS 4410</div>
+                                                     <div class="hexagon">CS 3110</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagon">CS 2110</div>
+                                                     <div class="hexagon">CS 4410</div>
+                                                     <div class="hexagon">CS 3110</div>
+                                                     <div class="hexagonLeft">CS 3410</div>
+                                                     <div class="hexagon">CS 3410</div>
+                                                     <div class="hexagon">CS 4410</div>
+                                                     <div class="hexagon">CS 3110</div>
 					    </div>
 					</div>
-					<div class="item">
-					    <div class= "classContainer"> 
-						<div class= "classholder"> Hello2 </div>
-					    </div>
-					</div>
-					<div class="item">
-					    <div class= "classContainer"> 
-						<div class= "classholder"> Hello3 </div>
-					    </div>
-					</div>
-				</div> <a class="left carousel-control" href="#carousel-144942" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+					');
+                                   }
+                                          ?>
+                               </div> <a class="left carousel-control" href="#carousel-144942" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
 				<a class="right carousel-control" href="#carousel-144942" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 			</div>
 		    </div>
